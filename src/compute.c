32213e7f03fcf9579035686d9b723a93b56f94f7
Ehab Saleh <ehab.saleh@lrz.de>
2026-06-09 09:14:50 +0200

    linearize 2d data structure

--- src/compute.c
+++ src/compute.c
@@ -3,15 +3,13 @@
 #include"compute.h"
 
 
-float **a=NULL;
+float *a=NULL;
 float *x=NULL;
 float *y=NULL;
 
 void init_arrays(void){
 	int i,j;
-	a=(float**)malloc(DIM*sizeof(float*));
-	for(i=0;i<DIM;i++)
-		a[i]=(float*)malloc(DIM*sizeof(float));
+	a=(float*)malloc(DIM*DIM*sizeof(float));
 
 	x=(float*)malloc(DIM*sizeof(float));
 	y=(float*)malloc(DIM*sizeof(float));
@@ -19,12 +17,12 @@ void init_arrays(void){
 	for(i=0;i<DIM;i++){
 		x[i]=1;
 		y[i]=1;
-
-		for(j=0;j<DIM;j++){
-			a[i][j]=2;
-		}
-	
 	}
+
+	for(j=0;j<DIM*DIM;j++){
+		a[j]=2;
+	}
+	
 
 }
 
@@ -37,7 +35,7 @@ void compute_on_host(double latency){
 		ccompute_start=MPI_Wtime();
 		for(i=0;i<DIM;i++)
 			for(j=0;j<DIM;j++)
-				x[i]=x[i]+a[i][j]*a[i][j]+y[j];
+				x[i]=x[i]+a[i*DIM+j]*a[i*DIM+j]+y[j];
 
 		ccompute_total+=MPI_Wtime()-ccompute_start;
 	}
