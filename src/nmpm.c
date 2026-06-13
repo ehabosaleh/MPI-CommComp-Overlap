b534ec76d68485129bb38529fe9031f19b78b9d8
Ehab Saleh <ehab.saleh@lrz.de>
2026-06-13 08:36:55 +0200

    enable send/recv in 3d dimention

--- src/nmpm.c
+++ src/nmpm.c
@@ -4,95 +4,141 @@
 #include<stdio.h>
 #include<compute.h>
 
-int run_overlap_benchmark(int rank, int size){
+int run_overlap_benchmark(int rank, int size, int dim){
 		int iter;
     	double t_pure=0, t_pure_total=0;
     	double tcomp=0, tcomp_total=0;
     	double t_ovrl=0, t_ovrl_total=0;
     	double overlap=0, overlap_avr=0;
     	double t_pure_total0=0, tcomp_total0=0, t_ovrl_total0=0;
-		int left=0,right=0,top=0,bottom=0;
-		int dims[2], coords[2];
+		int dims[3], coords[3];
+		int is_3d=0;
+		int num_neighbors;
 
 		MPI_Comm_rank(MPI_COMM_WORLD,&rank);
     	MPI_Comm_size(MPI_COMM_WORLD,&size);
+		if(dim==3){
 
-    	int sqrt_size = (int)sqrt((double)size);
-    	if (sqrt_size * sqrt_size != size) {
-        	if (rank == 0) {
-            		fprintf(stderr, "Number of processes must be a perfect square\n");
-        	}
-        MPI_Finalize();
-        return -1;
-    	}
+			int cbrt_size = (int)cbrt((double)size);
+			if (cbrt_size * cbrt_size * cbrt_size != size) {
+				if (rank == 0) {
+					fprintf(stderr, "Number of processes must be a perfect cube for 3D grid\n");
+				}
+				MPI_Finalize();
+				return -1;
+			}
 
-    	dims[0]=dims[1]=sqrt_size;
-
-    	coords[0]=rank/sqrt_size;
-    	coords[1]=rank%sqrt_size;
-
-    	left=(coords[1]==0)? MPI_PROC_NULL:rank-1;
-    	right=(coords[1]==dims[1]-1)?MPI_PROC_NULL:rank+1;
-    	top=(coords[0]==0)? MPI_PROC_NULL:rank-sqrt_size;
-    	bottom=(coords[0]==dims[0]-1)?MPI_PROC_NULL:rank + sqrt_size;
+			is_3d = 1;
+			dims[0] = dims[1] = dims[2] = cbrt_size;
+			coords[0] = rank / (cbrt_size * cbrt_size);
+			coords[1] = (rank % (cbrt_size * cbrt_size)) / cbrt_size;
+			coords[2] = rank % cbrt_size;
+			num_neighbors = 6;
+			}
+		else if(dim==2){
+			int sqrt_size = (int)sqrt((double)size);
+			if (sqrt_size * sqrt_size != size) {
+				if (rank == 0) {
+					fprintf(stderr, "Number of processes must be a perfect square or perfect cube\n");
+				}
+				MPI_Finalize();
+				return -1;
+			}
+			dims[0] = dims[1] = sqrt_size;
+			dims[2] = 1;
+			coords[0] = rank / sqrt_size;
+			coords[1] = rank % sqrt_size;
+			coords[2] = 0;
+			num_neighbors = 4;
+		}
 
     	init_arrays();
 
     	if (rank==0) {
+			if (is_3d) {
+				printf("Running 3D benchmark with grid %dx%dx%d\n", dims[0], dims[1], dims[2]);
+			} else {
+				printf("Running 2D benchmark with grid %dx%d\n", dims[0], dims[1]);
+			}
         	printf("%-20s%-20s%-20s%-20s%-20s\n","Size (Bytes)","Communication(us)","Computation(us)","Overall","Overlapping %");
     	}
 
-    	MPI_Request *reqs=(MPI_Request*)malloc(2*size*sizeof(MPI_Request));
+    	MPI_Request *reqs=(MPI_Request*)malloc(2*num_neighbors*sizeof(MPI_Request));
     	for (long local_N=MIN_MESSAGE_SIZE;local_N <= MAX_MESSAGE_SIZE; local_N *= 2) {
 
-        	char *u_0      = (char *)malloc(local_N);
-        	char *u_1      = (char *)malloc(local_N);
-        	char *u_2      = (char *)malloc(local_N);
-        	char *u_3      = (char *)malloc(local_N);
-        	char *u_left   = (char *)malloc(local_N);
-        	char *u_right  = (char *)malloc(local_N);
-        	char *u_top    = (char *)malloc(local_N);
-        	char *u_bottom = (char *)malloc(local_N);
-
-        	for(long i=0;i<local_N;i++) {
-            		u_0[i]      = 'a';
-            		u_1[i]      = 'b';
-            		u_2[i]      = 'c';
-            		u_3[i]      = 'd';
-            		u_left[i]   = 'f';
-            		u_right[i]  = 'g';
-            		u_top[i]    = 'h';
-            		u_bottom[i] = 'i';
+        	char *send_buffers[6];
+        	char *recv_buffers[6];
+        	
+			for (int i = 0; i < 6; i++) {
+        		send_buffers[i] = (char *)malloc(local_N);
+        		recv_buffers[i] = (char *)malloc(local_N);
+        		for (long j = 0; j < local_N; j++) {
+            		send_buffers[i][j] = 'a' + i;
+            		recv_buffers[i][j] = 'a' + i;
+        		}
         	}
 
         	t_pure=0;
         	t_pure_total=0;
         	tcomp_total=0;
         	t_ovrl_total=0;
-
-        	/* Phase-1: Pure communication time */
-        	for (iter =0;iter< MAX_ITER;iter++){
+        	for (iter =0; iter< MAX_ITER; iter++){
             		int req_count=0;
             		double init_time=MPI_Wtime();
+            		if (is_3d) {
+						int left = (coords[2]==0) ? MPI_PROC_NULL : rank - 1;
+						int right = (coords[2]==dims[2]-1) ? MPI_PROC_NULL : rank + 1;
+						int front = (coords[1]==0) ? MPI_PROC_NULL : rank - dims[2];
+						int back = (coords[1]==dims[1]-1) ? MPI_PROC_NULL : rank + dims[2];
+						int bottom = (coords[0]==dims[0]-1) ? MPI_PROC_NULL : rank + dims[2]*dims[1];
+						int top = (coords[0]==0) ? MPI_PROC_NULL : rank - dims[2]*dims[1];
 
-            		if(left !=MPI_PROC_NULL) {
-                		MPI_Isend(u_0, local_N, MPI_CHAR, left, 0, MPI_COMM_WORLD, &reqs[req_count++]);
-                		MPI_Irecv(u_left, local_N, MPI_CHAR, left, 0, MPI_COMM_WORLD, &reqs[req_count++]);
-            		}
+						if(left != MPI_PROC_NULL) {
+							MPI_Isend(send_buffers[0], local_N, MPI_CHAR, left, 0, MPI_COMM_WORLD, &reqs[req_count++]);
+							MPI_Irecv(recv_buffers[0], local_N, MPI_CHAR, left, 0, MPI_COMM_WORLD, &reqs[req_count++]);
+						}
+						if(right != MPI_PROC_NULL) {
+							MPI_Isend(send_buffers[1], local_N, MPI_CHAR, right, 0, MPI_COMM_WORLD, &reqs[req_count++]);
+							MPI_Irecv(recv_buffers[1], local_N, MPI_CHAR, right, 0, MPI_COMM_WORLD, &reqs[req_count++]);
+						}
+						if(front != MPI_PROC_NULL) {
+							MPI_Isend(send_buffers[2], local_N, MPI_CHAR, front, 0, MPI_COMM_WORLD, &reqs[req_count++]);
+							MPI_Irecv(recv_buffers[2], local_N, MPI_CHAR, front, 0, MPI_COMM_WORLD, &reqs[req_count++]);
+						}
+						if(back != MPI_PROC_NULL) {
+							MPI_Isend(send_buffers[3], local_N, MPI_CHAR, back, 0, MPI_COMM_WORLD, &reqs[req_count++]);
+							MPI_Irecv(recv_buffers[3], local_N, MPI_CHAR, back, 0, MPI_COMM_WORLD, &reqs[req_count++]);
+						}
+						if(top != MPI_PROC_NULL) {
+							MPI_Isend(send_buffers[4], local_N, MPI_CHAR, top, 0, MPI_COMM_WORLD, &reqs[req_count++]);
+							MPI_Irecv(recv_buffers[4], local_N, MPI_CHAR, top, 0, MPI_COMM_WORLD, &reqs[req_count++]);
+						}
+						if(bottom != MPI_PROC_NULL) {
+							MPI_Isend(send_buffers[5], local_N, MPI_CHAR, bottom, 0, MPI_COMM_WORLD, &reqs[req_count++]);
+							MPI_Irecv(recv_buffers[5], local_N, MPI_CHAR, bottom, 0, MPI_COMM_WORLD, &reqs[req_count++]);
+						}
+            		} else {
+						int left = (coords[1]==0) ? MPI_PROC_NULL : rank - 1;
+						int right = (coords[1]==dims[1]-1) ? MPI_PROC_NULL : rank + 1;
+						int top = (coords[0]==0) ? MPI_PROC_NULL : rank - dims[1];
+						int bottom = (coords[0]==dims[0]-1) ? MPI_PROC_NULL : rank + dims[1];
 
-            		if(right != MPI_PROC_NULL) {
-                		MPI_Isend(u_1, local_N, MPI_CHAR, right, 0, MPI_COMM_WORLD, &reqs[req_count++]);
-                		MPI_Irecv(u_right, local_N, MPI_CHAR, right, 0, MPI_COMM_WORLD, &reqs[req_count++]);
-            		}
-
-            		if(top != MPI_PROC_NULL) {
-                		MPI_Isend(u_2, local_N, MPI_CHAR, top, 0, MPI_COMM_WORLD, &reqs[req_count++]);
-                		MPI_Irecv(u_top, local_N, MPI_CHAR, top, 0, MPI_COMM_WORLD, &reqs[req_count++]);
-            		}
-
-            		if(bottom!= MPI_PROC_NULL) {
-                		MPI_Isend(u_3, local_N, MPI_CHAR, bottom, 0, MPI_COMM_WORLD, &reqs[req_count++]);
-                		MPI_Irecv(u_bottom, local_N, MPI_CHAR, bottom, 0, MPI_COMM_WORLD, &reqs[req_count++]);
+						if(left != MPI_PROC_NULL) {
+							MPI_Isend(send_buffers[0], local_N, MPI_CHAR, left, 0, MPI_COMM_WORLD, &reqs[req_count++]);
+							MPI_Irecv(recv_buffers[0], local_N, MPI_CHAR, left, 0, MPI_COMM_WORLD, &reqs[req_count++]);
+						}
+						if(right != MPI_PROC_NULL) {
+							MPI_Isend(send_buffers[1], local_N, MPI_CHAR, right, 0, MPI_COMM_WORLD, &reqs[req_count++]);
+							MPI_Irecv(recv_buffers[1], local_N, MPI_CHAR, right, 0, MPI_COMM_WORLD, &reqs[req_count++]);
+						}
+						if(top != MPI_PROC_NULL) {
+							MPI_Isend(send_buffers[2], local_N, MPI_CHAR, top, 0, MPI_COMM_WORLD, &reqs[req_count++]);
+							MPI_Irecv(recv_buffers[2], local_N, MPI_CHAR, top, 0, MPI_COMM_WORLD, &reqs[req_count++]);
+						}
+						if(bottom != MPI_PROC_NULL) {
+							MPI_Isend(send_buffers[3], local_N, MPI_CHAR, bottom, 0, MPI_COMM_WORLD, &reqs[req_count++]);
+							MPI_Irecv(recv_buffers[3], local_N, MPI_CHAR, bottom, 0, MPI_COMM_WORLD, &reqs[req_count++]);
+						}
             		}
 
             		MPI_Waitall(req_count, reqs, MPI_STATUSES_IGNORE);
@@ -110,25 +156,63 @@ int run_overlap_benchmark(int rank, int size){
         	for (iter = 0; iter < MAX_ITER; iter++) {
             		int req_count = 0;
             		double init_time = MPI_Wtime();
-            		if(left != MPI_PROC_NULL) {
-                		MPI_Isend(u_0, local_N, MPI_CHAR, left, 0, MPI_COMM_WORLD, &reqs[req_count++]);
-                		MPI_Irecv(u_left, local_N, MPI_CHAR, left, 0, MPI_COMM_WORLD, &reqs[req_count++]);
-            		}
+            		
+            		if (is_3d) {
+						int left = (coords[2]==0) ? MPI_PROC_NULL : rank - 1;
+						int right = (coords[2]==dims[2]-1) ? MPI_PROC_NULL : rank + 1;
+						int front = (coords[1]==0) ? MPI_PROC_NULL : rank - dims[2];
+						int back = (coords[1]==dims[1]-1) ? MPI_PROC_NULL : rank + dims[2];
+						int bottom = (coords[0]==dims[0]-1) ? MPI_PROC_NULL : rank + dims[2]*dims[1];
+						int top = (coords[0]==0) ? MPI_PROC_NULL : rank - dims[2]*dims[1];
 
-            		if(right!= MPI_PROC_NULL) {
-                		MPI_Isend(u_1, local_N, MPI_CHAR, right, 0, MPI_COMM_WORLD, &reqs[req_count++]);
-                		MPI_Irecv(u_right, local_N, MPI_CHAR, right, 0, MPI_COMM_WORLD, &reqs[req_count++]);
-            		}
+						if(left != MPI_PROC_NULL) {
+							MPI_Isend(send_buffers[0], local_N, MPI_CHAR, left, 0, MPI_COMM_WORLD, &reqs[req_count++]);
+							MPI_Irecv(recv_buffers[0], local_N, MPI_CHAR, left, 0, MPI_COMM_WORLD, &reqs[req_count++]);
+						}
+						if(right != MPI_PROC_NULL) {
+							MPI_Isend(send_buffers[1], local_N, MPI_CHAR, right, 0, MPI_COMM_WORLD, &reqs[req_count++]);
+							MPI_Irecv(recv_buffers[1], local_N, MPI_CHAR, right, 0, MPI_COMM_WORLD, &reqs[req_count++]);
+						}
+						if(front != MPI_PROC_NULL) {
+							MPI_Isend(send_buffers[2], local_N, MPI_CHAR, front, 0, MPI_COMM_WORLD, &reqs[req_count++]);
+							MPI_Irecv(recv_buffers[2], local_N, MPI_CHAR, front, 0, MPI_COMM_WORLD, &reqs[req_count++]);
+						}
+						if(back != MPI_PROC_NULL) {
+							MPI_Isend(send_buffers[3], local_N, MPI_CHAR, back, 0, MPI_COMM_WORLD, &reqs[req_count++]);
+							MPI_Irecv(recv_buffers[3], local_N, MPI_CHAR, back, 0, MPI_COMM_WORLD, &reqs[req_count++]);
+						}
+						if(top != MPI_PROC_NULL) {
+							MPI_Isend(send_buffers[4], local_N, MPI_CHAR, top, 0, MPI_COMM_WORLD, &reqs[req_count++]);
+							MPI_Irecv(recv_buffers[4], local_N, MPI_CHAR, top, 0, MPI_COMM_WORLD, &reqs[req_count++]);
+						}
+						if(bottom != MPI_PROC_NULL) {
+							MPI_Isend(send_buffers[5], local_N, MPI_CHAR, bottom, 0, MPI_COMM_WORLD, &reqs[req_count++]);
+							MPI_Irecv(recv_buffers[5], local_N, MPI_CHAR, bottom, 0, MPI_COMM_WORLD, &reqs[req_count++]);
+						}
+            		} else {
+						int left = (coords[1]==0) ? MPI_PROC_NULL : rank - 1;
+						int right = (coords[1]==dims[1]-1) ? MPI_PROC_NULL : rank + 1;
+						int top = (coords[0]==0) ? MPI_PROC_NULL : rank - dims[1];
+						int bottom = (coords[0]==dims[0]-1) ? MPI_PROC_NULL : rank + dims[1];
 
-            		if(top!=MPI_PROC_NULL) {
-                		MPI_Isend(u_2, local_N, MPI_CHAR, top, 0, MPI_COMM_WORLD, &reqs[req_count++]);
-                		MPI_Irecv(u_top, local_N, MPI_CHAR, top, 0, MPI_COMM_WORLD, &reqs[req_count++]);
+						if(left != MPI_PROC_NULL) {
+							MPI_Isend(send_buffers[0], local_N, MPI_CHAR, left, 0, MPI_COMM_WORLD, &reqs[req_count++]);
+							MPI_Irecv(recv_buffers[0], local_N, MPI_CHAR, left, 0, MPI_COMM_WORLD, &reqs[req_count++]);
+						}
+						if(right != MPI_PROC_NULL) {
+							MPI_Isend(send_buffers[1], local_N, MPI_CHAR, right, 0, MPI_COMM_WORLD, &reqs[req_count++]);
+							MPI_Irecv(recv_buffers[1], local_N, MPI_CHAR, right, 0, MPI_COMM_WORLD, &reqs[req_count++]);
+						}
+						if(top != MPI_PROC_NULL) {
+							MPI_Isend(send_buffers[2], local_N, MPI_CHAR, top, 0, MPI_COMM_WORLD, &reqs[req_count++]);
+							MPI_Irecv(recv_buffers[2], local_N, MPI_CHAR, top, 0, MPI_COMM_WORLD, &reqs[req_count++]);
+						}
+						if(bottom != MPI_PROC_NULL) {
+							MPI_Isend(send_buffers[3], local_N, MPI_CHAR, bottom, 0, MPI_COMM_WORLD, &reqs[req_count++]);
+							MPI_Irecv(recv_buffers[3], local_N, MPI_CHAR, bottom, 0, MPI_COMM_WORLD, &reqs[req_count++]);
+						}
             		}
-
-            		if(bottom!= MPI_PROC_NULL) {
-                		MPI_Isend(u_3, local_N, MPI_CHAR, bottom, 0, MPI_COMM_WORLD, &reqs[req_count++]);
-                		MPI_Irecv(u_bottom, local_N, MPI_CHAR, bottom, 0, MPI_COMM_WORLD, &reqs[req_count++]);
-            		}
+            		
             		double tcomp_start = MPI_Wtime();
             		compute_on_host(t_pure_total/1e6);
             		tcomp = MPI_Wtime()-tcomp_start;
@@ -170,14 +254,10 @@ int run_overlap_benchmark(int rank, int size){
         	tcomp_total0=0;
         	t_ovrl_total0=0;
 
-        	free(u_0);
-        	free(u_1);
-        	free(u_2);
-        	free(u_3);
-        	free(u_left);
-        	free(u_right);
-        	free(u_top);
-        	free(u_bottom);
+        	for (int i = 0; i < 6; i++) {
+        		free(send_buffers[i]);
+        		free(recv_buffers[i]);
+        	}
     	}
 
     	free(reqs);
