eb367e157f6ed995d32fb29c9bbe91728ad2007e
Ehab Saleh <ehab.saleh@lrz.de>
2026-06-14 07:24:16 +0200

    define compute bound step

--- src/compute.c
+++ src/compute.c
@@ -26,6 +26,17 @@ void init_arrays(void){
 
 }
 
+static void compute_bound_step(const long N){
+	double s0=0.0, s1=0.0, s2=0.0, s3=0.0;
+	for(int i=0;i<N;i++){
+		s0=s0*1.0000001+s1;
+		s1=s1*1.0000002+s2;
+		s2=s2*1.0000003+s3;
+		s3=s3*1.0000003+s0;
+	}
+	
+}
+
 void compute_on_host(double latency){
 	int i,j;
 	double ccompute_start=0;
@@ -33,9 +44,12 @@ void compute_on_host(double latency){
 	
 	while(ccompute_total<latency){
 		ccompute_start=MPI_Wtime();
+		/*
 		for(i=0;i<ARRAY_DIM;i++)
 			for(j=0;j<ARRAY_DIM;j++)
 				x[i]=x[i]+a[i*ARRAY_DIM+j]*a[i*ARRAY_DIM+j]+y[j];
+		*/
+		compute_bound_step(1000);
 
 		ccompute_total+=MPI_Wtime()-ccompute_start;
 	}
