0d2e3b876e83d42c6cfec30819e8409a3603cf6b
Ehab Saleh <ehab.saleh@lrz.de>
2026-06-17 09:47:06 +0200

    consider dev=gpu

--- src/main/main.c
+++ src/main/main.c
@@ -1,18 +1,24 @@
 #include "nmpm.h"
 
 int main(int argc, char *argv[]){
-    int dim = DIM; 
+    int dim = DIM;
+    int dev=0;
     int compToPureCommRatio=COMP_TO_COMM_RATIO;
 
     for(int i=0;i<argc;i++){
         if(strncmp(argv[i],"--dim=",6)==0 ){
             dim=atoi(argv[i]+6);
             if(dim!=2 && dim!=3&& dim!=1){
-                if(i==0){
-                    fprintf(stderr, "Invalid dimension specified. Use 1 for 1D grid, 2 for 2D grid or 3 for 3D grid.\n");
-                }
+                fprintf(stderr, "Invalid dimension specified. Use 1 for 1D grid, 2 for 2D grid or 3 for 3D grid.\n");
                 return -1;
             }
+        }
+        else if(strncasecmp(argv[i],"--dev=",6)==0){
+            dev=atoi(argv[i]+6);
+            if(dev!=0||dev!=1){
+                fprintf(stderr, "Invalid device specified. Use 0 for CPU or 1 for GPU.\n");
+            }
+
         }
         else if(strncmp(argv[i],"--ratio=",8)==0){
             compToPureCommRatio=atoi(argv[i]+8);
@@ -33,8 +39,10 @@ int main(int argc, char *argv[]){
     MPI_Comm_size(MPI_COMM_WORLD,&size);
 	MPI_Comm_rank(MPI_COMM_WORLD,&rank);
 
-    run_overlap_benchmark(rank,size,dim,compToPureCommRatio);
-
+    if(dev==0)
+        run_overlap_benchmark(rank,size,dim,compToPureCommRatio);
+    else if(dev==1)
+        run_overlap_benchmark_gpu(rank,size,dim,compToPureCommRatio)
     MPI_Finalize();
     return 0;
 }
