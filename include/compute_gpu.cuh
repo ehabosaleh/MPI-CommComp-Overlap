fdecd711fd874ee62c0c39a51b37d6b4049330f3
Ehab Saleh <ehab.saleh@lrz.de>
2026-06-19 11:07:17 +0200

    remove overloaded compute_on_gpu

--- include/compute_gpu.cuh
+++ include/compute_gpu.cuh
@@ -45,7 +45,6 @@ int calibrate_inner_iter(float *d_a, cudaStream_t stream,int grid, int block,siz
 
  double compute_on_gpu(float*d_a, cudaStream_t stream, int grid, int block, size_t n, double latency_us,double unit_us, int inner_iters);
 __global__ double compute_kernel(float *d_a, size_t n);
-double compute_on_gpu(float*d_a, cudaStream_t stream, int grid, int block, size_t n, double latency_us);
 void init_vector(int n);
 void free_vector(void);
 #ifdef __cplusplus
