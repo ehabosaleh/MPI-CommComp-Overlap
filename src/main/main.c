ec2ec0ecd7cb1c0a4553dec6e9cc703d75e70e3a
Ehab Saleh <ehab.saleh@lrz.de>
2026-07-08 01:31:41 +0200

    add MPI_Init_thread when progress thread enabled

--- src/main/main.c
+++ src/main/main.c
@@ -115,7 +115,19 @@ int main(int argc, char *argv[]){
     }
 
 	int rank, size;
-    MPI_Init(&argc, &argv);
+    if(do_progress){
+        int provided;
+        MPI_Init_thread(&argc, &argv, MPI_THREAD_MULTIPLE, &provided);
+
+        if(provided < MPI_THREAD_MULTIPLE) {
+            fprintf(stderr, "MPI_THREAD_MULTIPLE not supported\n");
+            MPI_Abort(MPI_COMM_WORLD, 1);
+        }
+    }
+    else{
+        MPI_Init(&argc, &argv);
+    }
+    
     MPI_Comm_size(MPI_COMM_WORLD,&size);
 	MPI_Comm_rank(MPI_COMM_WORLD,&rank);
 
