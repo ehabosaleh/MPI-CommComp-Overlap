a871527d91a54059f4c64ac30883919672cbf7ae
Ehab Saleh <ehab.saleh@lrz.de>
2026-07-09 13:05:10 +0200

    set cuda device when enabling progress thread

--- src/nmpm.c
+++ src/nmpm.c
@@ -374,6 +374,7 @@ int run_overlap_benchmark_gpu(int rank, int size, int dim, int compToPureCommRat
 	if(do_progress&&enable_thread){
 			start_progress_thread(&progress_data);
 			progress_data.is_thread=1;
+			progress_data.cuda_device=local_rank%device_count;
 	}
 	else
 		progress_data.is_thread=0;
