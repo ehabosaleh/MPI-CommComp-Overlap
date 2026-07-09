6bff03c3717f6d3a360838cdf8b98da8cce41a24
Ehab Saleh <ehab.saleh@lrz.de>
2026-07-09 10:29:16 +0200

    include STREAM types of memory operations

--- src/arithmetic-intensity/cpu-memory-bound.c
+++ src/arithmetic-intensity/cpu-memory-bound.c
@@ -1,6 +1,12 @@
 #include<stdlib.h>
 #include<stdio.h>
 
+typedef enum {
+    MEMORY_MODE_TRIAD=0,
+    MEMORY_MODE_COPY=1,
+    MEMORY_MODE_SCALE=2,
+    MEMORY_MODE_ADD=3
+} memory_mode_t;
 
 #if defined(__GNUC__) || defined(__clang__)
 #define NOINLINE __attribute__((noinline))
@@ -21,6 +27,7 @@ double *mb_a=NULL;
 double *mb_b=NULL;
 double *mb_c=NULL;
 size_t mb_elems=0;
+
 
 int init_memory_bound_buffers(size_t bytes){
 
@@ -55,7 +62,7 @@ void free_memory_bound_buffers(void){
     mb_elems = 0;
 }
 
-NOINLINE void cpu_memory_bound_batch(void){
+NOINLINE void cpu_memory_bound_batch(memory_mode_t memory_mode){
     if (mb_a == NULL || mb_b == NULL || mb_c == NULL || mb_elems == 0) {
         fprintf(stderr, "Memory-bound buffers are not initialized.\n");
         return;
@@ -71,20 +78,58 @@ NOINLINE void cpu_memory_bound_batch(void){
 
     size_t start = mb_offset;
     size_t end   = mb_offset + chunk;
-
-    for (size_t i = start; i < end; i++) {
-        mb_c[i] = mb_a[i] + A * mb_b[i];
+    switch (memory_mode) {
+        case MEMORY_MODE_TRIAD:
+            for (size_t i = start; i < end; i++) {
+                mb_c[i] = mb_a[i] + A * mb_b[i];
+            }
+            break;
+        case MEMORY_MODE_COPY:
+            for (size_t i = start; i < end; i++) {
+                mb_c[i] = mb_a[i];
+            }
+            break;
+        case MEMORY_MODE_SCALE:
+            for (size_t i = start; i < end; i++) {
+                mb_c[i] = A * mb_a[i];
+            }
+            break;
+        case MEMORY_MODE_ADD:
+            for (size_t i = start; i < end; i++) {
+                mb_c[i] = mb_a[i] + mb_b[i];
+            }
+            break;
+        default:
+            fprintf(stderr, "Invalid memory mode specified.\n");
+            return;
     }
 
     mb_offset += chunk;
 
     host_sink += mb_c[end - 1];
 }
-int main(void){
+int main(int argc,char**argv){
+
+    char *mode_str=argv[1];
+    memory_mode_t mode=MEMORY_MODE_TRIAD;
+    if(strcmp(mode_str,"triad")==0){
+        mode=MEMORY_MODE_TRIAD;
+    }else if(strcmp(mode_str,"copy")==0){
+        mode=MEMORY_MODE_COPY;
+    }else if(strcmp(mode_str,"scale")==0){
+        mode=MEMORY_MODE_SCALE;
+    }else if(strcmp(mode_str,"add")==0){
+        mode=MEMORY_MODE_ADD;
+    }else{
+        fprintf(stderr,"Error: unknown memory mode %s\n",mode_str);
+        return 1;
+    }   
+
 	init_memory_bound_buffers(256UL*1024UL*1024UL);
 	
 	for(int i=0;i<1000000;i++)
-		cpu_memory_bound_batch();
+		cpu_memory_bound_batch(mode);
+        
 	free_memory_bound_buffers();
 
 }
