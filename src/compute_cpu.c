7e48bcf2043bb7635b0965dd22e3faab01f32d8d
Ehab Saleh <ehab.saleh@lrz.de>
2026-07-10 06:44:08 +0200

    disable cuda

--- src/compute_cpu.c
+++ src/compute_cpu.c
@@ -208,7 +208,7 @@ int start_progress_thread(progress_thread_data_t *progress_data) {
     progress_data->num_requests = 0;
     if(progress_data->is_gpu) {
         printf("Cuda device was set for dev %d\n",progress_data->cuda_device);
-        cudaSetDevice(progress_data->cuda_device);
+        //cudaSetDevice(progress_data->cuda_device);
     } else {
         progress_data->cuda_device = -1;
     }
