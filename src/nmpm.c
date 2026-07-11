4759efb4427e43a88b98f93b92643eea1f869c3c
Ehab Saleh <ehab.saleh@lrz.de>
2026-07-11 19:21:34 +0200

    edit target target_unit_us

--- src/nmpm.c
+++ src/nmpm.c
@@ -321,7 +321,7 @@ int run_overlap_benchmark_gpu(int rank, int size, int dim, int compToPureCommRat
 	
 	
 	if(compute_bound==1){
-		gpu_inner_iters=calibrate_inner_iter(d_a,stream,grid,block,VECTOR_DIM_COMP,1,&measured_unit_us);
+		gpu_inner_iters=calibrate_inner_iter(d_a,stream,grid,block,VECTOR_DIM_COMP,50,&measured_unit_us);
 	}
 	else{
 		mem_cal=calibrate_memory_bound_kernel(d_c,d_a,d_b,stream,grid,block,elems_per_pass,max_elems,50,memory_mode);
