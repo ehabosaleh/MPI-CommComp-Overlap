4ddfbb8249ebdd6a40ccaaa04e41ce959a5e24ac
Ehab Saleh <ehab.saleh@lrz.de>
2026-08-05 11:46:35 +0200

    update SKIP/MAX iterations

--- include/nmpm.h
+++ include/nmpm.h
@@ -20,11 +20,11 @@ int run_overlap_benchmark_gpu(int rank, int size, int dim, int compToPureCommRat
 
 
 #ifndef MAX_ITER
-#define MAX_ITER 1000
+#define MAX_ITER 100
 #endif
 
 #ifndef SKIP
-#define SKIP 100
+#define SKIP 10
 #endif
 
 
